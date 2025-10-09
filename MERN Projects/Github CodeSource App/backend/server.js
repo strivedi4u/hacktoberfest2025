a8526e8ef315973ce9d63311fe4f2d0e76840d5a
Ruth Gicheha <60509305+Raiynegicheha@users.noreply.github.com>
2025-10-09 13:22:25 +0300

    Added README, .env.example, and fixed typos

--- MERN Projects/Github CodeSource App/backend/server.js
+++ MERN Projects/Github CodeSource App/backend/server.js
@@ -5,11 +5,17 @@ import cors from "cors";
 import userRoutes from "./routes/user.route.js";
 import exploreRoutes from "./routes/explore.route.js";
 
-dotenv.config();
+// dotenv.config();
+dotenv.config({ path: './backend/.env' });
 
 const app = express();
 
 app.use(cors());
+
+// ✅ Use PORT from .env or default to 5000
+
+const PORT = process.env.PORT || 5000;
+
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
@@ -18,6 +24,6 @@ app.get("/", (req, res) => {
 app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
 
-app.listen(5000, () => {
-  console.log("Server started on https://localhost:5000");
+app.listen(PORT, () => {
+  console.log("Server started on http://localhost:5000");
 });
