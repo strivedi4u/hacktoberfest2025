22dbb8c5f5bc34f10d1f1d01fd6b516b215dc8b1
Laxmi Amruta Padhi <164632284+laxmiamrutapadhi@users.noreply.github.com>
2025-10-14 20:27:33 +0530

    Update TowerOfHanoi.cpp

--- cpp/TowerOfHanoi.cpp
+++ cpp/TowerOfHanoi.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
-using namespace std;
 
-void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod) {
+void towerOfHanoi(int n, const char from_rod, const char to_rod, const char aux_rod, int &moveCount) {
     if (n == 1) {
-        cout << "Move disk 1 from rod " << from_rod << " to rod " << to_rod << endl;
+        std::cout << "Move disk 1 from rod " << from_rod << " to rod " << to_rod << std::endl;
+        moveCount++;
         return;
     }
-    towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
-    cout << "Move disk " << n << " from rod " << from_rod << " to rod " << to_rod << endl;
-    towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+    towerOfHanoi(n - 1, from_rod, aux_rod, to_rod, moveCount);
+    std::cout << "Move disk " << n << " from rod " << from_rod << " to rod " << to_rod << std::endl;
+    moveCount++;
+    towerOfHanoi(n - 1, aux_rod, to_rod, from_rod, moveCount);
 }
 
 int main() {
     int n;
-    cout << "Enter number of disks: ";
-    cin >> n;
-    towerOfHanoi(n, 'A', 'C', 'B');
+    std::cout << "Enter number of disks: ";
+    std::cin >> n;
+
+    int moveCount = 0;
+    towerOfHanoi(n, 'A', 'C', 'B', moveCount);
+
+    std::cout << "Total moves: " << moveCount << std::endl;
     return 0;
 }
