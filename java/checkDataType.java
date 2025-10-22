9f7fcad4ae513712794b3ee24c852b4c8ec14fc2
Tannu Singh <you@example.com>
2025-10-22 23:55:34 +0530

    feat(powerbi): add fashion_boutique_dataset.xlsx, initial Power BI report and documentation for Excel->PowerBI flow

--- java/checkDataType.java
+++
@@ -1,9 +0,0 @@
-public class checkDataType {
-    public checkDataType() {
-        System.out.println("DataTypeChecker object created.");
-    }
-
-    public static void checkDataType(Object obj) {
-        System.out.println("The type is: " + (obj == null ? "null" : obj.getClass().getSimpleName()));
-    }
-}
