18ec823f6cb06c51238e4aab3c30c8040b0b9df2
kris2005mehra <krismehra22@gmail.com>
2025-10-24 00:39:43 +0530

    Added SleepCircle project files.

--- java/factorial.java
+++ java/factorial.java
@@ -1,41 +1,18 @@
 import java.util.Scanner;
-
-// Class to calculate factorial
-class FactorialCalculator {
-    
-    // Method to calculate factorial
-    public int calculateFactorial(int number) {
-        int factorial = 1;
-        
-        // Loop to calculate factorial
-        for (int i = 1; i <= number; i++) {
-            factorial *= i;
-        }
-        
-        return factorial;
-    }
+public class Factorial
+{
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter an Integer: ");
+int num = sc.nextInt();
+int fact = 1;
+if(num < 0)
+System.out.println("Invalid Input");
+else
+{
+for(int i = 1; i <= num; i++)
+fact = fact * i;
+System.out.print("Factorial is "+fact);
 }
-
-// Main class
-public class FactorialMain {
-    public static void main(String[] args) {
-        // Create scanner object to take input
-        Scanner scanner = new Scanner(System.in);
-        
-        // Create object of FactorialCalculator class
-        FactorialCalculator calculator = new FactorialCalculator();
-        
-        // Take user input
-        System.out.print("Enter a number to calculate its factorial: ");
-        int number = scanner.nextInt();
-        
-        // Calculate factorial using the class method
-        int result = calculator.calculateFactorial(number);
-        
-        // Print the result
-        System.out.println("The factorial of " + number + " is: " + result);
-        
-        // Close scanner
-        scanner.close();
-    }
+}
 }
