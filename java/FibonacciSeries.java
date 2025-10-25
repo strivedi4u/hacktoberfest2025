2930e51fae752723636da8e9b8843c598bbdde46
Rattan Singh <182332870+rattan-dev@users.noreply.github.com>
2025-10-25 23:05:27 +0530

    Refactor Fibonacci series to accept user input
    Updated Fibonacci series implementation to take user input for the number of terms and corrected the logic for generating the series.

--- java/FibonacciSeries.java
+++ java/FibonacciSeries.java
@@ -1,15 +1,13 @@
-class Fibonacci{  
-public static void main(String args[])  
-{    
- int n1=0,n2=1,n3,i,count=10;    
- System.out.print(n1+" "+n2);//printing 0 and 1    
-    
- for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
- {    
-  n3=n1+n2;    
-  System.out.print(" "+n3);    
-  n1=n2;    
-  n2=n3;    
- }    
-  
-}}  
+import java.util.Scanner;
+class FibonacciSeries{
+ public static void main(String[] args){
+  int first=0, second=1, third = 1, n;
+  Scanner sc = new Scanner(System.in);
+  System.out.print("Enter a number: "); // number of terms in the series
+  n = sc.nextInt(); // taking input
+  for(int i =0; i<n; i++){
+   System.out.print(first+" ");
+   first = second; second = third; third = first + second;
+  }
+ }
+}
